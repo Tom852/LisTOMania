e1fa76370bb0b5a4b7552004d72b0e4cd7b80b7f
Tom <thomas.kistler@ost.ch>
2023-10-13 09:20:57 +0200

    readd stie.js

---
+++ ListTOMania.Web/wwwroot/js/site.js
@@ -0,0 +1,12 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+
+
+function handleChecked(e, listId, itemId)  {
+    let isChecked = e.target.checked;
+    console.log(isChecked)
+    console.log(listId)
+    console.log(itemId)
+}
